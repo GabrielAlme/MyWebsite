ac03f7b9bb1a56af3b81c6817097532cf2bad89a
GabrielAlme <gabealmeemail@gmail.com>
2026-04-13 13:38:10 -0500

    i c an spel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <p>2026 Gabriel Alme</p>
             <div className="footer-links">
                 <a href="https://github.com/GabrielAlme" target="_blank">My Github</a>
-                <a href="https://linkedin.com/in/gabriel-alme-80b2a4266" target="_blank">My Linkedin (careful they steal ur data)</a>
+                <a href="https://linkedin.com/in/gabriel-alme-80b2a4266" target="_blank">My Linkedin (careful they steal your data)</a>
             </div>
         </footer>
     )

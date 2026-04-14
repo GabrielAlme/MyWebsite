043cdc9b57198b3e9f1daae0dad116aa8781cdd7
GabrielAlme <gabealmeemail@gmail.com>
2026-04-13 19:55:46 -0500

    created basic projects page and stylesheet

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import './projects.css'
+
+function Projects() {
+    return(
+        <div className="projects">
+            <h1>These are my Projects:</h1>
+            <div className="projects-list">
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Projects

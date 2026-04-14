b4730d356436468d1b3fd4450b291931ddf6f6b9
GabrielAlme <gabealmeemail@gmail.com>
2026-04-13 19:47:41 -0500

    removed gabe on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import './Navbar.css'
 function Navbar() {
     return (
         <nav>
-            <div className="nav-logo">Gabe</div>
             <ul className="nav-links">
                 <li><Link to="/">Home</Link></li>
                 <li>Projects</li>

7d289f3b316192f40af63503b23979ef350ee7cd
GabrielAlme <gabealmeemail@gmail.com>
2026-04-14 21:59:34 -0500

    added basic inputs to the leave a message page

--- src/components/LeaveMsg.jsx
+++ src/components/LeaveMsg.jsx
@@ -1,8 +1,51 @@
 import './LeaveMsg.css'
+import emailjs from '@emailjs/browser'
+import '.Leavemsg.css'
+
+const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+})
+
+
+
 
 function LeaveMsg() {
     return(
         <>
+        <div className="form">
+            <div className="name">
+                <input
+                    required
+                    type="text"
+                    name="name"
+                    placeholder="Your name (required)"
+                />
+            </div>
+            <div className="email">
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Your email"
+                />
+            </div>
+            <div className="phone">
+                <input
+                    type="text"
+                    name="phone"
+                    placeholder="Your phone number"
+                />
+            </div>
+            <div className="Message">
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Leave your message here (required)"
+                />
+            </div>
+        </div>
         </>
     )
 }

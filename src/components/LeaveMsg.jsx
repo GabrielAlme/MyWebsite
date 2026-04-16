3bef0acaaab7da01a49df4146c720400eca9223e
GabrielAlme <gabealmeemail@gmail.com>
2026-04-15 23:11:57 -0500

    im going to bed

--- src/components/LeaveMsg.jsx
+++ src/components/LeaveMsg.jsx
@@ -11,6 +11,27 @@ function LeaveMsg() {
     message: ''
     })
 
+    const [error, setError] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const phoneDigits = form.phone.replace(/\D/g, '')
+        if (form.phone.length > 0 && phoneDigits.length < 10) {
+            setError('Please enter a valid phone number')
+            return
+        }
+
+        emailjs.send(
+            'service_l6aetea',
+            'template_58g28x4',
+            form,
+            '1-NZrbnEWtOknam5d'
+        ) .then(() => {
+            setShowModal(true)
+        })
+    }
+
     const handlePhone = (e) => {
         let value = e.target.value.replace(/\D/g, '')
         if (value.length > 10) value = value.slice(0, 10)
